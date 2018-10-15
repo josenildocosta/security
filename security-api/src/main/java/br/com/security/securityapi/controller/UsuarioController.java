ce6c7d156616373c3329b26989a72335b369da0b
Josenildo Costa <josenildocosta@gmail.com>
2018-10-14 22:33:38 -0300

    Teste do pdfmaker

--- security-api/src/main/java/br/com/security/securityapi/controller/UsuarioController.java
+++ security-api/src/main/java/br/com/security/securityapi/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.security.securityapi.model.Usuario;
 import br.com.security.securityapi.repository.UsuarioRepository;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class UsuarioController {
 	@Autowired
